7ee04d188d105d43e7807a74b07510019169e4c5
Marcin Szabłowski <marcin.szablowski@op.pl>
2019-10-26 23:55:17 +0200

    Add generate password functionality

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const lengthEl = document.querySelector("#length");
+const upperEl = document.querySelector("#uppercase");
+const lowerEl = document.querySelector("#lowercase");
+const symbolsEl = document.querySelector("#symbols");
+const numbersEl = document.querySelector("#numbers");
+const generateEl = document.querySelector(".btn");
+const passwordEl = document.querySelector("#password");
+const copyEl = document.querySelector(".copy-btn");
+
+const randomFunc = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  numbers: getRandomNumber,
+  symbols: getRandomSymbol
+}
+
 // Generator functions
 
 function getRandomLower() {
@@ -18,3 +34,61 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+//Events
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasUpper = upperEl.checked;
+  const hasLower = lowerEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  passwordEl.value = generatePassword(hasUpper, hasLower, hasNumber, hasSymbols, length);
+})
+
+//Generate password
+
+function generatePassword(upper, lower, numbers, symbols, length) {
+  let password = "";
+  const typesCount = upper + lower + numbers + symbols;
+  const typesArr = [{ upper }, { lower }, { numbers }, { symbols }].filter
+  (
+    item => Object.values(item)[0]
+  );
+
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i=0; i<length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      password += randomFunc[funcName]();
+    })
+  }
+
+  const finalPassword = password.slice(0, length);
+  return finalPassword;
+
+}
+
+
+// Copy password
+copyEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const pass = passwordEl.value;
+
+  if (!pass) {
+    console.log("!");
+    return;
+  } 
+
+  textarea.value = pass;
+
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  console.log("Copied!");
+});
